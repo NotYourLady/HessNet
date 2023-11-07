b8a73a58eef9f6e5d84d6f0992d7f983e3aff583
Mikhail <mihailshutov105@gmail.com>
2023-11-07 22:28:42 +0300

    update code

--- build/lib/SegRunLib/run.py
+++
@@ -1,8 +0,0 @@
-from SegRunLib.scripts.RunnerClass import Runner
-
-def run(in_path_nifty: str, out_path_nifty: str, settings=None):
-    runner = Runner(settings=settings)
-    runner.predict_and_save(in_path_nifty, out_path_nifty)
-    
-    
-    
